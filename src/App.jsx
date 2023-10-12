c81cd2a4f4d39a77ef26c310b8ec6617af35f556
Sébastien LAMBRANCA <116831255+Lambseb@users.noreply.github.com>
2023-10-12 15:33:52 +0200

    sauvegarde des mod dans src/app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
-import data from 
+// import { useState } from "react";
+import data from "./data/data.json";
+// import Navbar from "./component/navbar";
+// import CardList from "./component/Cardlist";
+import Card from "./component/Card";
+// import Footer from "./component/footer";
 
 function App() {
-
+ 
   return (
-   
-  )
+    <>
+      
+      <Card data={data} />
+    </>
+  );
 }
-export default App
+export default App;
